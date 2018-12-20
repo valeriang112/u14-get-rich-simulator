a6fecfe4339b9abdc080e08983039d0c6543e115
Valerian Garleanu <valerian.garleanu@ada.ac.uk>
2018-12-20 09:32:27 +0000

    fixed directory structures once again

--- js/entities/index.js
+++
@@ -1,8 +0,0 @@
-import player_entity from './player_entity.js';
-import menu_entity from './menu_entity.js';
-import new_game_entity from './new_game_entity.js';
-import actual_game from './actual_game.js';
-import cutscene_1 from './cutscene_1.js';
-
-const entities = {"PlayerEntity": player_entity, "MenuEntities": menu_entity, "NewgameEntity": new_game_entity, "Cutscene1": cutscene_1, "ActualGame": actual_game};
-export default entities;
